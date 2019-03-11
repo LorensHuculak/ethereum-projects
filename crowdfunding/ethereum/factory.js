1f91348c225ebf3a3e611fb698dc0b2a6de20e76
LorensHuculak <huculaklorens@yahoo.com>
2019-03-11 18:08:48 +0100

    Next.js Setup

--- crowdfunding/ethereum/factory.js
+++ crowdfunding/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xCA7740C40E82f945D4e48b9Cf2475c2674B2813D'
+  '0xe9b79BE3744EF879aA34C004A18b97D469757fB5'
 );
 
 export default instance;
